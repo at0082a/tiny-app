ab735efb95904e3fb2fe85aefcea435660e27efb
alex <you@example.com>
2019-04-09 20:54:47 +0000

    created urls_show ejs file

--- express_server.js
+++ express_server.js
@@ -18,7 +18,8 @@ app.get("/urls", (req, res) => {
   });
 
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    console.log(templateVars)
     res.render("urls_show", templateVars);
 });
 
